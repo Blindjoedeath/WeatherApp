ed623946630f33d8490cb162c3a881237b561e0a
Оскар Моисеев <blindjoedeath0@gmail.com>
2018-11-01 15:49:47 +0300

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Blind Joe Death on 01/11/2018.
+//  Copyright © 2018 Codezavod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
