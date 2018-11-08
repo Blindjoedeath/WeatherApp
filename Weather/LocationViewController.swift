f3c1d6252bbfa93421ab1d413eedaa42d1130316
Оскар Моисеев <blindjoedeath0@gmail.com>
2018-11-08 13:47:17 +0300

    Implemented text field protocol

--- Weather/LocationViewController.swift
+++ Weather/LocationViewController.swift
@@ -15,10 +15,15 @@ class LocationViewController: UIViewController {
     @IBOutlet weak var cityTextField: UITextField!
     @IBOutlet weak var defineLocationButton: UIButton!
     @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var darkView: UIView!
+    
+    private let weatherRequest = WeatherRequest()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        cityTextField.borderStyle = .roundedRect
         setDate()
+        addGestureRecognizer()
     }
     
     func setDate(){
@@ -34,3 +39,52 @@ class LocationViewController: UIViewController {
     }
 }
 
+extension LocationViewController: UITextFieldDelegate{
+    
+    @objc
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    func setDarkViewState(_ state : Bool){
+        darkView.isHidden = !state
+        
+        var alpha = 0.0
+        if (state){
+            alpha = 0.4
+        }
+        
+        UIView.animate(withDuration: 0.2, delay: 0.0, options:[], animations: {
+            self.darkView.backgroundColor =  UIColor(red: 0, green: 0, blue: 0, alpha: CGFloat(alpha))
+        }, completion:nil)
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        setDarkViewState(true)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        setDarkViewState(false)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        dismissKeyboard()
+        return true
+    }
+}
+
+
+extension LocationViewController: UIGestureRecognizerDelegate {
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer,
+                           shouldReceive touch: UITouch) -> Bool {
+        return (touch.view === darkView)
+    }
+    
+    func addGestureRecognizer(){
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        gestureRecognizer.cancelsTouchesInView = false
+        gestureRecognizer.delegate = self
+        view.addGestureRecognizer(gestureRecognizer)
+    }
+}
