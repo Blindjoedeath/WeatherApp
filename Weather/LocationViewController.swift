f9219d13e94cf9079a76a30edcd8db2a1cfff613
Оскар Моисеев <blindjoedeath0@gmail.com>
2018-11-12 15:13:06 +0300

    Added locating

--- Weather/LocationViewController.swift
+++ Weather/LocationViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class LocationViewController: UIViewController {
 
@@ -17,10 +18,21 @@ class LocationViewController: UIViewController {
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var darkView: UIView!
     
+    
+    private var updatingLocation = false
+    private var locationError: NSError?
+    private var location: CLLocation?
+    private let locationManager = CLLocationManager()
+    private let geocoder = CLGeocoder()
+    private var placemark: CLPlacemark?
+    private var timer: Timer?
+    
     private let weatherRequest = WeatherRequest()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager.delegate = self
         cityTextField.borderStyle = .roundedRect
         setDate()
         addGestureRecognizer()
@@ -86,5 +98,121 @@ extension LocationViewController: UIGestureRecognizerDelegate {
         gestureRecognizer.cancelsTouchesInView = false
         gestureRecognizer.delegate = self
         view.addGestureRecognizer(gestureRecognizer)
+    }
+}
+
+extension LocationViewController: CLLocationManagerDelegate{
+    
+    // MARK: - CLLocationManagerDelegate
+    func locationManager(_ manager: CLLocationManager,
+                         didFailWithError error: Error) {
+        locationError = error as NSError
+        stopLocationManager()
+    }
+    
+    func locationManager(_ manager: CLLocationManager,
+                         didUpdateLocations locations: [CLLocation]) {
+        let newLocation = locations.last!
+        
+        if newLocation.timestamp.timeIntervalSinceNow < -5
+            || newLocation.horizontalAccuracy < 0 {
+            return
+        }
+        
+        if location == nil || newLocation.horizontalAccuracy < location!.horizontalAccuracy {
+            locationError = nil
+            location = newLocation
+            
+            if newLocation.horizontalAccuracy <= 65 {
+                stopLocationManager()
+                geocoder.reverseGeocodeLocation(newLocation, completionHandler: {
+                    placemarks, error in
+                    if error == nil, let p = placemarks, !p.isEmpty {
+                        self.placemark = p.last!
+                        print("found")
+                    } else {
+                        self.locationError = error as NSError?
+                        self.placemark = nil
+                    }
+                })
+            }
+        }
+    }
+    
+    @IBAction func getLocation() {
+        var authStatus = CLLocationManager.authorizationStatus()
+        if authStatus == .notDetermined {
+            locationManager.requestWhenInUseAuthorization()
+            return
+        }
+        
+        if authStatus == .denied || authStatus == .restricted {
+            showLocationServicesDeniedAlert()
+            return
+        }
+        
+        if !updatingLocation{
+            startLocationManager()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        if (status == .notDetermined){
+            return
+        }
+        if (status == .denied || status == .restricted) {
+            showLocationServicesDeniedAlert()
+        } else {
+            getLocation()
+        }
+    }
+    
+    func showLocationServicesDeniedAlert() {
+        let alert = UIAlertController(title: "Службы геолокации выключены",
+                                      message:
+            "Пожалуйста, включите службы геолокации в настройках.",
+                                      preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default,
+                                     handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func startLocationManager() {
+        if CLLocationManager.locationServicesEnabled() {
+            location = nil
+            placemark = nil
+            locationError = nil
+            updatingLocation = true
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+            timer = Timer.scheduledTimer(timeInterval: 20, target: self,
+                                         selector: #selector(didTimeOut),
+                                         userInfo: nil, repeats: false)
+        }
+        else{
+            print("Disabled")
+        }
+    }
+    
+    func stopLocationManager() {
+        if updatingLocation {
+            locationManager.stopUpdatingLocation()
+            updatingLocation = false
+            
+            if let timer = timer {
+                timer.invalidate()
+            }
+        }
+    }
+    
+    @objc
+    func didTimeOut() {
+        if location == nil {
+            stopLocationManager()
+            locationError = NSError(domain: "Time out error",
+                                        code: 1, userInfo: nil)
+        }
     }
 }
